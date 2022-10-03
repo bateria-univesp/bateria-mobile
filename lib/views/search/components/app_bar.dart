2734722c01f2ae6dcc042541163a17c7da222098
Lucas Viana <lv201122@gmail.com>
2022-10-03 00:18:24 -0300

    Add search by location button

--- lib/views/search/components/app_bar.dart
+++ lib/views/search/components/app_bar.dart
@@ -1,3 +1,4 @@
+import 'package:bateria_mobile/views/search/components/search_by_location_button.dart';
 import 'package:bateria_mobile/views/search/components/search_by_text_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -10,6 +11,7 @@ class SearchPageAppBar extends ConsumerWidget implements PreferredSizeWidget {
     return AppBar(
       title: const Text('Bateria'),
       actions: const [
+        SearchByLocationButton(),
         SearchByTextButton(),
       ],
     );
