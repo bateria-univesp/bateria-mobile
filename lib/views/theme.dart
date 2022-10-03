b99ee68ad42d0aee8f9706ceec886c61b91caa09
Lucas Viana <lv201122@gmail.com>
2022-10-02 23:23:44 -0300

    Secondary change: update app theme

--- lib/views/theme.dart
+++ lib/views/theme.dart
@@ -4,7 +4,12 @@ class BateriaTheme {
   static ThemeData buildTheme() {
     return ThemeData(
       colorScheme: ColorScheme.fromSwatch(
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.red,
+        cardColor: Colors.red[50],
+      ),
+      appBarTheme: const AppBarTheme(
+        elevation: 8,
+        shadowColor: Colors.black,
       ),
       useMaterial3: true,
     );
