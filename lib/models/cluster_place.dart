30ad6fec0935d4be4b30801a83ca2571b57a33b1
Lucas Viana <lv201122@gmail.com>
2022-10-02 22:48:49 -0300

    Rename Place to ClusterPlace

--- lib/models/cluster_place.dart
+++ lib/models/cluster_place.dart
@@ -2,15 +2,16 @@ import 'package:bateria_mobile/models/collect_point.dart';
 import 'package:google_maps_cluster_manager/google_maps_cluster_manager.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class Place with ClusterItem {
+class ClusterPlace with ClusterItem {
   final String title;
   final String subtitle;
   final LatLng latLng;
 
-  Place({required this.title, required this.subtitle, required this.latLng});
+  ClusterPlace(
+      {required this.title, required this.subtitle, required this.latLng});
 
-  factory Place.fromCollectPoint(CollectPoint collectPoint) {
-    return Place(
+  factory ClusterPlace.fromCollectPoint(CollectPoint collectPoint) {
+    return ClusterPlace(
         title: collectPoint.name,
         subtitle: collectPoint.address,
         latLng: LatLng(collectPoint.latitude, collectPoint.longitude));
