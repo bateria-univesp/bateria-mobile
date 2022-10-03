8876cef144b7d952f7812d5c9a5bab4f42fac5b8
Lucas Viana <lv201122@gmail.com>
2022-10-02 23:32:25 -0300

    Remove Google API key
    It's been deprecated in Google Cloud Console as well.

--- lib/infrastructure/google_maps_api/configuration.dart
+++ lib/infrastructure/google_maps_api/configuration.dart
@@ -1,6 +1,6 @@
 class GoogleMapsApiConfiguration {
   static const apiKey = String.fromEnvironment(
     'MAPS_API_KEY',
-    defaultValue: 'AIzaSyC8SKu0RwkYDGz5S6ev5xzyDW7YBBn9nY8',
+    defaultValue: '',
   );
 }
