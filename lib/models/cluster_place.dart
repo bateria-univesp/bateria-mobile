4aa7d92e492bf934f33baf047376c4a184468284
Lucas Viana <lv201122@gmail.com>
2022-10-07 21:36:15 -0300

    Update ClusterPlace class to hold a `T data` property

--- lib/models/cluster_place.dart
+++ lib/models/cluster_place.dart
@@ -2,19 +2,25 @@ import 'package:bateria_mobile/models/collect_point.dart';
 import 'package:google_maps_cluster_manager/google_maps_cluster_manager.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class ClusterPlace with ClusterItem {
+class ClusterPlace<T> with ClusterItem {
   final String title;
   final String subtitle;
   final LatLng latLng;
+  final T data;
 
   ClusterPlace(
-      {required this.title, required this.subtitle, required this.latLng});
+      {required this.title,
+      required this.subtitle,
+      required this.latLng,
+      required this.data});
 
-  factory ClusterPlace.fromCollectPoint(CollectPoint collectPoint) {
+  static ClusterPlace<CollectPoint> fromCollectPoint(
+      CollectPoint collectPoint) {
     return ClusterPlace(
         title: collectPoint.name,
         subtitle: collectPoint.address,
-        latLng: LatLng(collectPoint.latitude, collectPoint.longitude));
+        latLng: LatLng(collectPoint.latitude, collectPoint.longitude),
+        data: collectPoint);
   }
 
   @override
